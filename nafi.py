4c13c48826ca620f1a8d546dcbda1fa3fa5b2219
Alide-Bro <163498253+Alide-Bro@users.noreply.github.com>
2024-03-15 11:24:27 +0600

    Add files via upload

---
+++ nafi.py
@@ -0,0 +1,11 @@
+print("1: ADD")
+print("2: SUBSTRACT")
+print("3: MULTIPLY")
+print("4: EVEN")
+print("5: SQURE")
+
+operations= input()
+
+if operations == "1":
+    Num1 = int(input("First Number: "))
+    Num2 = int(input("Second Number: "))
